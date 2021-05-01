9208bb7392fa8d895054c7d742ac329a65382334
Carrie Kuhlman <carrie.kuhlman@gmail.com>
2021-05-01 22:23:23 +0000

    Fixed create_org function to not required org_description and org_website.

--- crud.py
+++ crud.py
@@ -22,15 +22,17 @@ def create_donor(fname, lname, user_id):
 
     return donor
 
-def create_org(org_name, org_description, org_website, user_id):
+def create_org(org_name, user_id, org_description=None, org_website=None):
     """Create and retun a new org."""
 
-    org = Org(org_name=org_name, org_description=org_description, org_website=org_website, user_id=user_id)
+    org = Org(org_name=org_name, user_id=user_id, org_description=org_description, org_website=org_website)
 
     db.session.add(org)
     db.session.commit()
 
     return org
+
+# def create_location(phone, street_address, city, state, zip_code, accept_in_person, donation_hours, org_id)
 
 
 
