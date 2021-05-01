be3b4f27cd6ec33536794aceca54f4cfe6f6cdcd
Carrie Kuhlman <carrie.kuhlman@gmail.com>
2021-05-01 21:53:09 +0000

    Created and tested create_user function.

--- crud.py
+++ crud.py
@@ -2,9 +2,18 @@
 
 from model import db, User, Donor, Org, Location, Item, connect_to_db
 
+def create_user(email, password): 
+    """Create and reurn a new user."""
+
+    user = User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
 
 
 
-if __name__ = "__main__": 
+if __name__ == "__main__": 
     from server import app
     connect_to_db(app)
