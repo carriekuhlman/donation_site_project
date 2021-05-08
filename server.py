8bb449ecbb7ba1aeb6bed44d14b2485b633b1eec
Carrie Kuhlman <carrie.kuhlman@gmail.com>
2021-05-08 21:52:52 +0000

    Updated server.py to include homepage route, added content to base.html, created homepage.html.

--- server.py
+++ server.py
@@ -1,9 +1,26 @@
 """Server for donations site."""
 
-from flask import Flask 
+from flask import (Flask, render_template, request, flash,
+                session, redirect)
+from model import connect_to_db
+import crud
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+@app.route('/')
+def homepage():
+    """View homepage."""
+
+    return render_template('homepage.html')
+
+
+
 if __name__ == "__main__": 
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
