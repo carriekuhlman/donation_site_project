c4ae1f3e6e9293f28e70e2ad65501fb785d7ec2d
Carrie Kuhlman <carrie.kuhlman@gmail.com>
2021-06-17 00:49:07 +0000

    Added donations.js

---
+++ static/js/donations.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Show item search results
+
+function replaceItems(results) {
+  $("#searched-item-info").html(results.)
+}
+
+function showResults(evt) {
+  evt.preventDefault();
+
+
+}
